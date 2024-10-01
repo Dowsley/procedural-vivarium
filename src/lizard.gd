4a7d3bccc48643e28d519db6fc3946eb2d44bd3b
Dowsley <joao.dowsley850@gmail.com>
2024-09-30 23:00:59 -0300

    Setting terrain for UI to help me stabilize this damn lizard

--- src/lizard.gd
+++ src/lizard.gd
@@ -10,6 +10,19 @@ class_name Lizard
 ]
 var arms_desired: Array[Vector2] = []
 
+var debug := false
+var m_arm_length1 := 52 / 4
+var m_arm_length2 := 36 / 4
+var m_joint_count := 3
+var m_body_index1 := 3
+var m_body_index2 := 7
+var m_angle1 := PI / 4
+var m_angle2 := PI / 3
+var m_len_offset_desired := 50
+var m_distance_thres := 100
+var m_len_offset_end := -5
+var m_fabrik_lerp_weight := 0.2
+
 
 func _ready() -> void:
 	spine.initialize(joint_count, link_size, angle_constraint)
@@ -19,8 +32,8 @@ func _ready() -> void:
 	arms_desired.resize(4)
 	for i in range(4):
 		arms_desired[i] = Vector2.ZERO
-		var arm_length := (52 if i < 2 else 36) / 4
-		arms[i].initialize(3, arm_length)
+		var arm_length := m_arm_length1 if i < 2 else m_arm_length2
+		arms[i].initialize(m_joint_count, arm_length)
 
 
 func _process(delta: float) -> void:
@@ -33,35 +46,37 @@ func _process(delta: float) -> void:
 
 	for i in range(arms.size()):
 		var side := 1 if i % 2 == 0 else -1
-		var body_index := 3 if i < 2 else 7
-		var angle := (PI / 4 if i < 2 else PI / 3)
+		var body_index := m_body_index1 if i < 2 else m_body_index2
+		var angle := m_angle1 if i < 2 else m_angle2
 		var angle_offset := angle * side
 		
 		var desired_pos := Vector2(
-			get_pos_x(body_index, angle_offset, 50),
-			get_pos_y(body_index, angle_offset, 50)
+			get_pos_x(body_index, angle_offset, m_len_offset_desired),
+			get_pos_y(body_index, angle_offset, m_len_offset_desired)
 		)
 		
-		if desired_pos.distance_to(arms_desired[i]) > 100:
+		if desired_pos.distance_to(arms_desired[i]) > m_distance_thres:
 			arms_desired[i] = desired_pos
 
 		var end_angle_offset := (PI / 2 * side)
 		var end_pos := Vector2(
-			get_pos_x(body_index, end_angle_offset, -5),
-			get_pos_y(body_index, end_angle_offset, -5)
+			get_pos_x(body_index, end_angle_offset, m_len_offset_end),
+			get_pos_y(body_index, end_angle_offset, m_len_offset_end)
 		)
 		
 		arms[i].fabrik_resolve(
-			arms[i].points[0].lerp(arms_desired[i], 0.2),
+			arms[i].points[0].lerp(arms_desired[i], m_fabrik_lerp_weight),
 			end_pos
 		)
 	queue_redraw()
 
 
-#func _draw() -> void:
-	#for i in range(arms.size()):
-		## Debugging: Uncomment the lines below to visualize arm targets
-		#draw_line(arms[i].points[0], arms_desired[i], Color.GREEN, 2)  # Target position
+func _draw() -> void:
+	if not debug:
+		return
+	for i in range(arms.size()):
+		# Debugging: Uncomment the lines below to visualize arm targets
+		draw_line(arms[i].points[0], arms_desired[i], Color.GREEN, 2)  # Target position
 
 
 func get_pos_x(i: int, angle_offset: float, length_offset: float) -> float:
